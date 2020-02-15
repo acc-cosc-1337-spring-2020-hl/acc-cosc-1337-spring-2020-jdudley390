44e28f7a7a4eb854f6107685b69a2e015041d369
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-14 23:45:10 -0600

    remove

--- out/build/x64-Debug/test/homework_test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Jon/source/repos/acc-cosc-1337-spring-2020-jdudley3902/test/homework_test
-# Build directory: C:/Users/Jon/source/repos/acc-cosc-1337-spring-2020-jdudley3902/out/build/x64-Debug/test/homework_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("01_expressions_test")
